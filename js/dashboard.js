9768592e0b196d7990857df343e46dda55f37a74
TeamProjectV <104442153+TeamProjectV@users.noreply.github.com>
2022-05-10 06:16:58 -0700

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,1 +1,20 @@
-..
+var icon = document.getElementById("icon")
+var bargraph = document.getElementById("bargraph")
+var piechart = document.getElementById ("piechart")
+
+
+function darkmode() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+  if (document.body.classList.contains("dark-mode")) {
+    document.getElementById("bargraph").src= "images/mostselled_black.png" ;
+    document.getElementById("piechart").src= "images/piechart_black.png" ;
+    document.getElementById("themebtn").className=  "fa-solid fa-toggle-on";
+  } 
+  else {
+    document.getElementById("bargraph").src= "images/mostselled.png" ;
+    document.getElementById("piechart").src= "images/piechart.png" ;
+    document.getElementById("themebtn").className=  "fa-solid fa-toggle-off";
+  }
+  
+}
