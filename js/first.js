5226ac391a18567da295ee1918cc5a0e1bd8114c
TeamProjectV <104442153+TeamProjectV@users.noreply.github.com>
2022-05-10 16:27:26 +0200

    Add files via upload

---
+++ js/first.js
@@ -0,0 +1,15 @@
+let changeicon = function (icon) {
+   
+    icon.classlist.toggle('fa-check-square')
+}
+changeicon = (icon) => icon.classList.toggle('fa-check-square');
+
+
+function myFunction() {
+    alert("item added to cart successfully");
+}
+
+function myFunction3() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
