5b4bfceda313b42de9961cb5cea0800c5e255781
TeamProjectV <104442153+TeamProjectV@users.noreply.github.com>
2022-05-12 01:15:19 +0200

    verena-js

--- js/bstyle.js
+++ js/bstyle.js
@@ -86,5 +86,11 @@
       },
     },
   });
+ var content = document.getElementsByTagName('body')[0];
+  var darkMode = document.getElementById('dark-change');
+  darkMode.addEventListener('click', function(){
+      darkMode.classList.toggle('active');
+      content.classList.toggle('night');
+  })
   
   
