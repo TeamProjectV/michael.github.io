a6e1c120b0f013f68ab094a469ca8a6387813cae
TeamProjectV <104442153+TeamProjectV@users.noreply.github.com>
2022-05-11 20:35:20 +0200

    Add files via upload

--- js/style.js
+++ js/style.js
@@ -10,24 +10,6 @@ $('. menu-togglr').on('click',function(){
     $(this).toggleclass('active');
 });
 });
-
-const search = () =>{
-    const searchbox = document.getElementsByClassName("search-area").value.toUpperCase();
-    const storeitems = document. getElementById("navigation")
-    const product = document.querySelectorAll("catbox")
-    const pname = document.getElementsByTagName("alt")
-    for(var i=0; i < alert.length; i++){
-        let match =product[i].getElementsByTagName('alt')[0];
-        if(match){
-            let textvalue = match.textContent || match.innerHTML
-            if(textvalue.toUpperCase().indexOf(searchbox) > -1){
-                product[i].getElementsByClassName.display = "";
-            }
-            else{
-                product[i].getElementsByClassName.display = "none";
-            }
-
-        }
-    }
-
+function myfunction(){
+    alert("item added successfully");
 }
