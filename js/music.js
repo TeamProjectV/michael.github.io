d7b11f325ee792a4deed9d3c095c33c66ee6a3ff
TeamProjectV <104442153+TeamProjectV@users.noreply.github.com>
2022-05-11 18:32:06 +0200

    Update music.js

--- js/music.js
+++ js/music.js
@@ -2,10 +2,3 @@ function myFunction() {
     var element = document.body;
     element.classList.toggle("theme");
 }
-function cardspace(){
-    var carddigit = document.getElementById('cardno').ariaValueMax;
-    if(carddigit.length ==4 || carddigit.length == 9 || carddigit.length == 14){
-        document.getElementById('cardno').value = carddigit+" ";
-        document.getElementById('cardno').max = 1;
-    }
-}
