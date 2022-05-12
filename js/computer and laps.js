f74fa041fb5b4bcfc3363b9f4db56db3e06e48e3
TeamProjectV <104442153+TeamProjectV@users.noreply.github.com>
2022-05-12 05:57:02 +0200

    Add files via upload

---
+++ js/computer and laps.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
+function myFunction2() {
+    var element = document.body;
+    element.classList.toggle("theme");
+}
