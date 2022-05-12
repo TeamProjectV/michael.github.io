97cf9f5257149e87655d4507a2347e549a97d3a5
TeamProjectV <104442153+TeamProjectV@users.noreply.github.com>
2022-05-12 02:05:15 +0200

    Add files via upload

--- js/style.js
+++ js/style.js
@@ -13,3 +13,34 @@ $('. menu-togglr').on('click',function(){
 function myfunction(){
     alert("item added successfully");
 }
+function message(){
+    var name = document.getElementById('name');
+    var email=document.getElementById('email');
+    var age = document.getElementById('age');
+    var phone =document.getElementById('phone');
+    var msg = document.getElementById('msg');
+    const success = document.getElementById('success');
+    const danger = document.getElementById('danger');
+    if(name.value === '' || email.value === '' || age.value ===''||  phone.value ===''|| msg.value ===''){
+        danger.style.display = 'block';
+    }
+    else{
+        setTimeout(() => {
+            name.value ='';
+            email.value ='';
+            age.value ='';
+            phone.value ='';
+            msg.value ='';
+        }, 2000);
+        success.style.display = 'block';
+    }
+
+
+    setTimeout(() => {
+
+    danger.style.display = 'none';
+    success.style.display = 'none';
+}, 4000);
+
+
+}
