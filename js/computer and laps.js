6fffe8203478114862ee901468f0e411149c4c2b
TeamProjectV <104442153+TeamProjectV@users.noreply.github.com>
2022-05-12 14:01:37 +0200

    Update computer and laps.js

--- js/computer and laps.js
+++ js/computer and laps.js
@@ -1,8 +1,12 @@
+let x = document.getElementByclassName("type_taital");
 function myFunction() {
     var element = document.body;
     element.classList.toggle("dark-mode");
+    
 }
 function myFunction2() {
     var element = document.body;
     element.classList.toggle("theme");
+  x.style.color = "white";
+    
 }
